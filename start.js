5ed4116d57559881647f31cf6cce99bdce0b4cff
Duff Zhang <duff830@gmail.com>
2014-07-09 15:01:10 +0800

    add taobao router

--- start.js
+++ start.js
@@ -2,10 +2,12 @@ var express = require('express');
 var config = require('./config');
 
 var serviceRouter = require('./lib/service_router');
+var taobaoRouter = require('./lib/taobao_router');
 
 var app = express();
 app.use('/rest', serviceRouter);
 app.use(express.static(__dirname + '/public'));
+app.use('/router/rest',taobaoRouter);
 
 var server = app.listen(config.server.port, function() {
 	console.log('Listening on port %d', server.address().port);
