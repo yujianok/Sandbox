86948ba7787c321420519f9360af524d322a428f
yujianok <yujianok@gmail.com>
2014-07-24 10:40:41 +0800

    cap deploy configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,12 +11,12 @@ set :deploy_via, :copy
 namespace :deploy do
 
    task :install, :roles => :web do
-       "cd ~/Sandbox/; bower install"
-       "cd ~/Sandbox/; npm install"
+       "cd ~/Sandbox/current; bower install"
+       "cd ~/Sandbox/current; npm install"
    end
 
    task :restart, :roles => :web do
-      "cd ~/Sandbox/; forever restart start.js"
+      "cd ~/Sandbox/current; forever restart start.js"
    end
 
 end
